075fc1d9f930d7df8396371868be54eb3ef0ee78
robinhood1712 <ademolaadewoleo1@gmail.com>
2022-05-03 17:45:50 +0800

    more fun practice

--- src/App.js
+++ src/App.js
@@ -1,35 +1,80 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import UserOutput from './userOutput';
-import UserInput from './userInput';
+import Person from './Persons/Persons';
+
+
 
 class App extends Component {
   state = {
-    username : 'adewole'
+    persons :[
+      { name: 'Ade', age: 30, id: 'qwerty'},
+      {name: 'korede', age: 35, id: 'lsjsj'},
+      {name: 'teni', age: 40, id: 'hssana'}
+    ],
+    otherState: 'some other value',
+    showPersons : false
   }
 
-  nameChangedHandler = (event) => {
+  switchNameHandler = (name) => {
       this.setState({
-        username: event.target.value
-      })
+        persons: [
+          { name: name, age: 30 },
+          { name: 'korede', age: 35 },
+          { name: 'teni', age: 40 }
+        ]
+      } )  
+  }
+
+  nameChangedHandler = ( event ) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    } )
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
   }
 
 
 
-  render() {
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} key={person.id}  />
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-
-
-        <UserOutput username = {this.state.username} />
-        <UserOutput username = ' yusuf'/>
-        <UserInput
-           changed = {this.nameChangedHandler}
-           name= {this.state.username}/>
-
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
