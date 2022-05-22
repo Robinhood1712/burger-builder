0e947e31cb38c9d3dffe723ff48de83df7990d65
robinhood1712 <ademolaadewoleo1@gmail.com>
2022-05-22 21:32:38 +0800

    creating a responsive sideDrawer component

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Logo from "../../../components/Logo/Logo";
+import NavigationItems from "..//NavigationItems/NavigationItems";
+
+import styles from "../SideDrawer/SideDrawer.css";
+
+
+const sideDrawer = () => {
+    return (
+        <div className={styles.SideDrawer}>
+            <Logo />
+            <nav>
+                <NavigationItems />
+            </nav>
+
+        </div>
+    )
+}
+
+
+export default sideDrawer;
