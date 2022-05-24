b6ee321169df1087aba06e3be66d5ae194fc9d0e
robinhood1712 <ademolaadewoleo1@gmail.com>
2022-05-24 16:47:44 +0800

    preventing unneccesary rendering of the order summary component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,40 +1,49 @@
-import React from "react";
+import React, {Component} from "react";
 
 import Auxilliary from "../../../hoc/auxi";
 
 import Button from "../../UI/Button/Button";
 
 
-const orderSummary = (props) => {
-    const ingredientsSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component{
+
+    componentWillUpdate() {
+        console.log('[orderSummary] willupdate')
+    }
+
+    render(){
+
+        const ingredientsSummary = Object.keys(this.props.ingredients)
         .map(igkey => {
             return (
                 <li key={igkey}>
-                    {igkey} : {props.ingredients[igkey]}
-                </li> );
-    });
+                    {igkey} : {this.props.ingredients[igkey]}
+                </li> )
+        });
+    
 
-
-    return (
-        <Auxilliary>
+        return (
+            <Auxilliary>
             <h3>Your Order</h3>
              <p>A delicious burger with the following ingredients :</p>
             <ul>
                 {ingredientsSummary}
             </ul>
             <p>Continue to checkout ?</p>
-            <h3><strong>Total Price: {props.price}</strong></h3>
-            <Button clicked = {props.purchaseCancelled} btnType = 'Danger'>
+            <h3><strong>Total Price: {this.props.price}</strong></h3>
+            <Button clicked = {this.props.purchaseCancelled} btnType = 'Danger'>
                 Cancel 
             </Button>
-            <Button clicked = {props.purchaseContinued} btnType = 'Success'>
+            <Button clicked = {this.props.purchaseContinued} btnType = 'Success'>
                 Continue ?
             </Button>
 
         </Auxilliary>
 
-    )
+        )
+    }
+
 }
 
 
-export default orderSummary;
+export default OrderSummary;
