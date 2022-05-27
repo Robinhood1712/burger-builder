0158726d40fb364c91242dcc956cf1411bf79c6c
robinhood1712 <ademolaadewoleo1@gmail.com>
2022-05-27 21:31:31 +0800

    using axios interceptors to intercept error messages and ouput it globally

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Auxilliary from "../auxi";
 import Modal from "../../components/UI/Modal/Modal";
+import axios from "../../Axios-orders";
 
 
-const withErrorHandler = (WrappedComponent) => {
-    return (props) => {
-        return (
-            <Auxilliary>
-                <Modal show>
-                    <p>someting went wrong!</p>
-                </Modal>
-                <WrappedComponent  {...props} />                
-            </Auxilliary>
-        ) 
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component{
+
+        state = {
+            error: null
+        }
+
+        componentDidMount(){
+            axios.interceptors.request.use(req => {
+                this.setState({
+                    error: null
+                })
+            })
+            axios.interceptors.response.use(null, error=> {
+                this.setState({error: error})
+            })
+        }
+
+
+        render (){
+            return (
+                <Auxilliary>
+                    <Modal show = {this.state.error}>
+                        <p>{this.state.error.message}</p>
+                    </Modal>
+                    <WrappedComponent  {...this.props} />                
+                </Auxilliary>
+            ) 
+        }
     }
-
 }
 
 
